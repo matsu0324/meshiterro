af048e22c2ea08bc117d0ac8eb11c774ae09fb5a
EC2 Default User <ec2-user@ip-172-31-21-57.ec2.internal>
2020-09-24 09:02:30 +0000

    [Add] 4-5 投稿削除機能、マイページ追加

--- app/models/post_image.rb
+++ app/models/post_image.rb
@@ -4,7 +4,7 @@ class PostImage < ApplicationRecord
   attachment :image
   has_many :post_comments, dependent: :destroy
   has_many :favorites, dependent: :destroy
-  
+
   validates :shop_name, presence: true
   validates :image, presence: true
 
